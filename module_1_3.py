6e36f0398519f7ce74a4884b6d15a99ac3537c81
Любовь <lt89@bk.ru>
2024-12-19 08:19:20 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+hero = True
+name = "Бэтмен"
+age = 31
+new_age = age + 3
+print(f"Имя персонажа: {name}")
+print(f"Возраст персонажа: {age}")
+print(f"Является ли {name} супергероем? {'Да' if hero else 'Нет'}")
+print(f"Через 3 года {name} будет {new_age} года.")
