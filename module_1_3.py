60944c1aa44fa7434423ed026eb8ec64c151bd1d
Любовь <lt89@bk.ru>
2025-01-27 14:25:01 +0300

    Delete module_1_3.py

--- module_1_3.py
+++
@@ -1,8 +0,0 @@
-hero = True
-name = "Бэтмен"
-age = 31
-new_age = age + 3
-print(f"Имя персонажа: {name}")
-print(f"Возраст персонажа: {age}")
-print(f"Является ли {name} супергероем? {'Да' if hero else 'Нет'}")
-print(f"Через 3 года {name} будет {new_age} года.")
